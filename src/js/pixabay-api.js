de9b703abd65dd95addf0fa7e9302bfb344a350b
OlgaKozoriz07 <161890591+OlgaKozoriz07@users.noreply.github.com>
2024-08-26 14:51:29 +0200

    change key

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,7 +2,7 @@ const BASE_URL = 'https://pixabay.com/api';
 
 export const fetchPhotos = searchedQuery => {
   const urlParams = new URLSearchParams({
-    key: '45531869-402fff4ce98a39e1a3b7e2442',
+    key: '45590965-234de3c6df0497a136de38750',
     q: searchedQuery,
     image_type: 'photo',
     orientation: 'horizontal',
